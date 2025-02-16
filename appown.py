03883be4e0c0ad504c7f05f92625f1cbf1f3319d
anuskm <prasadanu1021@gmail.com>
2025-02-16 14:29:03 +0530

    Update appown.py

--- appown.py
+++ appown.py
@@ -18,12 +18,12 @@ def predict_survival(d):
 st.title("Diabetes")
 
 # Input fields for each parameter
-Glucose = st.number_input("Glucose", min_value=0,max_value=200, index=76)
-BloodPressure = st.number_input("BloodPressure", min_value=0,max_value=122, index=0)
-Insulin = st.number_input("Insulin ", min_value=0.0, max_value=846, value=50.0, step=0.1)
-BMI= st.number_input("BMI", min_value=0, max_value=67, value=1)
-DiabetesPedigreeFunction= st.number_input("DiabetesPedigreeFunction", min_value=0, max_value=2, value=0)
-Age = st.number_input("Age", min_value=21, max_value=81, value=7.25, step=0.1)
+Glucose = st.number_input("Glucose", min_value=0,max_value=200, value=0)
+BloodPressure = st.number_input("BloodPressure", min_value=0,max_value=122, value=0)
+Insulin = st.number_input("Insulin ", min_value=0.0, max_value=846, value=50.0, value=0.)
+BMI= st.number_input("BMI", min_value=0, max_value=67, value=0.1)
+DiabetesPedigreeFunction= st.number_input("DiabetesPedigreeFunction", min_value=0, max_value=2, value=0.1)
+Age = st.number_input("Age", min_value=21, max_value=81, value=7.25, value=21)
 
 
 
